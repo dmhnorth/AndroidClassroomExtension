07eb3e01ade7d39a92089c51bd8b14bc22e41377
David North <dmhnorth@gmail.com>
2014-07-30 21:55:22 +0100

    Added the permission to write to the External SD within the Android manifest. This implicitly allows reads. Created the start of the profile picture taking facility within the teacher creator. It can currently load up and use the camera to grab a photo. But currently nothing is done with this photo. The aim will be to place it in the area where the current ImageView is. Aim will be to withdraw some of this method out into another java class to it can be reused within the student profile creation section.

--- app/src/main/java/com/ace/androidclassroomextension/CreateTeacher.java
+++ app/src/main/java/com/ace/androidclassroomextension/CreateTeacher.java
@@ -3,14 +3,24 @@ package com.ace.androidclassroomextension;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Picture;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.storage.StorageManager;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ace.androidclassroomextension.R;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * For creating all the assets necessary to instantiate a classroom
@@ -49,12 +59,79 @@ public class CreateTeacher extends Activity {
         return String.valueOf(descriptionET.getText());
     }
 
+
+    //Media format codes
+    public static final int MEDIA_FORMAT_IMAGE = 1;
+    public static final int MEDIA_FORMAT_VIDEO = 2;
+
+    //request result code for using image afterwards
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+
+    //Uri address to save the media
+    private Uri fileUri;
+
+
+
     public void selectPhoto(View view) {
-        //TODO Enable the ability to select a photo from the users handset
-        
 
+        //TODO move as much of image capture system (including above refs) to a new class and eliminate video returns
 
+        // Intent to take a picture then return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
+        // create an Immutable URI reference. to save the image
+        fileUri = getOutputMediaFileUri(MEDIA_FORMAT_IMAGE);
 
+        // set the image file name
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+        // start the image capture Intent
+        startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
     }
+
+
+
+    /** Create a file Uri for saving an image or video */
+    private static Uri getOutputMediaFileUri(int type){
+        return Uri.fromFile(getOutputMediaFile(type));
+    }
+
+    /**
+     * Create a timestamped media file on the SD card
+     * @param type
+     * @return the media file created
+     */
+    private static File getOutputMediaFile(int type){
+
+        //TODO Use returned String with details of the internal media storage status.
+        Environment.getExternalStorageState();
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "Ace");
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("Ace", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name using the date and type
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_FORMAT_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "IMG_"+ timeStamp + ".jpg");
+        } else if(type == MEDIA_FORMAT_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                    "VID_"+ timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+
 }
+
