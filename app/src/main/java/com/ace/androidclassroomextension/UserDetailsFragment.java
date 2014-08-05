a7a728ef2c113c0d0ed2b5873ad833b963aac894
David North <dmhnorth@gmail.com>
2014-08-05 15:42:36 +0100

    Started work on the new Teacher creator activity. Moving some of the old sections of the view into the new one. Along with creating a specific fragment for the user details to be displayed.

---
+++ app/src/main/java/com/ace/androidclassroomextension/UserDetailsFragment.java
@@ -0,0 +1,25 @@
+package com.ace.androidclassroomextension;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Dave on 05/08/2014.
+ */
+public class UserDetailsFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_user_details, container, false);
+        return view;
+    }
+}
