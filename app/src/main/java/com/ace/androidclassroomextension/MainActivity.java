f9f21321a5c73460c64137a306dece87954036fe
David North <dmhnorth@gmail.com>
2014-08-15 13:39:50 +0100

    Added a note for where the user is initialised.

--- app/src/main/java/com/ace/androidclassroomextension/MainActivity.java
+++ app/src/main/java/com/ace/androidclassroomextension/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ace.androidclassroomextension;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,16 +14,48 @@ import android.widget.Toast;
 import com.ace.androidclassroomextension.creatorActivities.CreateUser;
 import com.ace.androidclassroomextension.userTypes.User;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 
 public class MainActivity extends Activity {
 
     private EditText nameEntry;
     private User user;
 
+    private String PERSISTENCE_TEST = "Didn't work yet";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/**
+        //Load the persistent user data
+        FileInputStream fin = null;
+        try {
+            fin = openFileInput(USER_FILE);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (fin != null) {
+            try {
+                PERSISTENCE_TEST = String.valueOf(fin.read());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (fin != null) {
+            try {
+                fin.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+ **/
+        Log.d("Persistence: ", PERSISTENCE_TEST);
 
     }
 
@@ -41,8 +74,13 @@ public class MainActivity extends Activity {
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             Toast.makeText(this, "No settings yet!", Toast.LENGTH_SHORT).show();
+
+            //TODO delete this test
+            PERSISTENCE_TEST = "TEST PRESS WORKED";
+            Log.d("Persistence: ", PERSISTENCE_TEST);
+
             return true;
-        } else if (id == R.id.exitTheApp){
+        } else if (id == R.id.exitTheApp) {
             finish();
             return true;
         }
@@ -51,6 +89,7 @@ public class MainActivity extends Activity {
 
     /**
      * Gets the entered username
+     *
      * @return username
      */
     private String getUserName() {
@@ -60,6 +99,7 @@ public class MainActivity extends Activity {
 
     /**
      * Initiate the option and Activity for creating a User
+     *
      * @param view current view
      */
     public void createUser(View view) {
@@ -67,11 +107,11 @@ public class MainActivity extends Activity {
         //Validate name entry
         Log.d("current name: ", getUserName());
 
-        if (!(getUserName().equals(""))){
+        if (!(getUserName().equals(""))) {
             Intent createUserIntent = new Intent(this, CreateUser.class);
 
             //Initialise the user if it hasn't been done
-            if (user == null){
+            if (user == null) {
                 user = new User(getUserName());
             }
 
@@ -88,5 +128,18 @@ public class MainActivity extends Activity {
         super.onDestroy();
         //TODO implement this so the user is saved, may have to lose 'new' within createUser
 
+        //Save the persistent user data
+        try {
+
+            FileOutputStream fos = openFileOutput(PERSISTENCE_TEST, Context.MODE_PRIVATE);
+            fos.write(PERSISTENCE_TEST.getBytes());
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Log.i("onDestroy:","was called and persistance");
     }
 }
